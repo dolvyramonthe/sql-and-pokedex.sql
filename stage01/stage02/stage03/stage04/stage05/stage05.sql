d76ab6b948e8bdbf5c14381a5a61a8a216ff0edc
roseline <monthe_r@etna-alternance.net>
2023-11-06 14:39:19 +0100

    listes des 5 premieres attaques de ronflex

---
+++ stage01/stage02/stage03/stage04/stage05/stage05.sql
@@ -0,0 +1,6 @@
+SELECT moves.identifier AS attaque
+FROM pokemon
+JOIN pokemon_moves ON pokemon.id = pokemon_moves.pokemon_id
+JOIN moves ON pokemon_moves.move_id = moves.id
+WHERE pokemon.identifier = 'snorlax'
+LIMIT 5;
