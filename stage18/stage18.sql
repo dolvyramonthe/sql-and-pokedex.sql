33a960f575006a9f86fa8d9cfb1206b8bda7d488
roseline <monthe_r@etna-alternance.net>
2023-11-06 23:18:49 +0100

    inserer les trois pokemon

---
+++ stage18/stage18.sql
@@ -0,0 +1,5 @@
+INSERT INTO pokemon (id, identifier, species_id, height, weight, base_experience, `order`, is_default)
+VALUES
+    (11000, 'grookey', 810, 7, 56, 64, 1, 1),
+    (11001, 'scorbunny', 813, 3, 45, 63, 2, 1),
+    (11002, 'sobble', 816, 3, 45, 63, 3, 1);
