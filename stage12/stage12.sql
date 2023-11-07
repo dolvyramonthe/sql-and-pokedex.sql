cf32965dc36cd99a6db1350682e56b498eafb0ff
roseline <monthe_r@etna-alternance.net>
2023-11-07 10:16:39 +0100

    exercice 12

--- stage12/stage12.sql
+++ stage12/stage12.sql
@@ -1,6 +1,5 @@
-SELECT COUNT(pokemon.id) AS nb
-FROM pokemon
-INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id
-INNER JOIN types ON pokemon_types.type_id = types.id
-INNER JOIN pokemon_form_generations ON pokemon.id = pokemon_form_generations.pokemon_id
-WHERE types.identifier = 'steel' AND pokemon_form_generations.generation_id = 2;
+select count(*) as nb from pokemon p 
+inner join pokemon_types pt on p.id = pt.pokemon_id 
+inner join types t on t.id = pt.type_id 
+inner join pokemon_form_generations pfg on pfg.pokemon_form_id = p.id  
+where t.identifier = "Steel" and pfg.generation_id = 2  ;c
